78f4d82cbac4f42790ce3a65610d0956d71bee3c
rabithoy <emailforfrom@gmail.com>
2025-07-31 12:24:31 +0700

    Update 3.sh

--- 3.sh
+++ 3.sh
@@ -1,8 +1,8 @@
-﻿#!/bin/bash
+#!/bin/bash
 
 # 🧩 Bước 1: Tải nếu chưa có main.zip
 if [ ! -f "main.zip" ]; then
-  wget -O https://github.com/rabithoy/tth/archive/refs/heads/main.zip
+  wget -O main.zip https://github.com/rabithoy/tth/archive/refs/heads/main.zip
 fi
 
 # 🧩 Bước 2: Giải nén đè

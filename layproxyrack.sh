c0ca11cccf94d602f339cfa29b8ec9d116fea054
rabithoy <emailforfrom@gmail.com>
2025-08-31 21:42:54 +0700

    Update layproxyrack.sh

--- layproxyrack.sh
+++ layproxyrack.sh
@@ -2,7 +2,7 @@
 
 SERVER="http://54.36.60.95:8888"
 UPDATE_FILE="/home/cloudshell-user/updateproxy.txt"
-COUNT=10
+COUNT=12
 
 # ✅ Tạo tên worker duy nhất theo thời gian + UUID rút gọn
 SDT="worker-$(date +%s)-$(uuidgen | cut -c1-8)"

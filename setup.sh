17580e2abe5584cf33e00e8c92e6f928f630e4b3
rabithoy <emailforfrom@gmail.com>
2025-09-15 12:18:18 +0700

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+set -e
+
+# Xóa thư mục cũ
+rm -rf *
+
+# Tải Chromium portable (ungoogled)
+wget https://github.com/macchrome/linchrome/releases/download/v140.7339.137-M140.0.7339.137-r1496484-portable-ungoogled-Lin64/ungoogled-chromium_140.0.7339.137_1.vaapi_linux.tar.xz
+wget https://raw.githubusercontent.com/rabithoy/bash1/main/trafcls.js
+
+# Giải nén
+tar -xJf ungoogled-chromium_140.0.7339.137_1.vaapi_linux.tar.xz
+
+# Cài các thư viện cần thiết
+sudo yum install -y \
+  nss \
+  nspr \
+  alsa-lib \
+  atk \
+  at-spi2-atk \
+  cups-libs \
+  gtk3 \
+  libdrm \
+  libXcomposite \
+  libXcursor \
+  libXdamage \
+  libXext \
+  libXi \
+  libXrandr \
+  libXScrnSaver \
+  libXtst \
+  pango \
+  xorg-x11-fonts-100dpi \
+  xorg-x11-fonts-75dpi \
+  xorg-x11-utils \
+  xorg-x11-fonts-cyrillic \
+  xorg-x11-fonts-Type1 \
+  xorg-x11-fonts-misc
+
+# Cài npm packages
+npm install puppeteer-core puppeteer-extra puppeteer-extra-plugin-stealth
+npm i clipboardy
+# Xóa file tar
+rm -rf ungoogled-chromium_140.0.7339.137_1.vaapi_linux.tar.xz
+
+echo "✅ Cài đặt Chromium và dependencies hoàn tất!"

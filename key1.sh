97c49ce97bd49d2b5b38c7a3dd9867a9644adb58
rabithoy <emailforfrom@gmail.com>
2025-10-07 13:44:57 +0700

    Update key1.sh

--- key1.sh
+++ key1.sh
@@ -3,19 +3,6 @@
 GROUP_ID="all"
 
 while true; do
-  echo "Đang ping server để lấy appToken..."
-
-  # Gọi API để lấy appToken
-  APP_TOKEN_JSON=$(curl -s "http://142.171.114.6:6000/worker-ping?groupId=$GROUP_ID")
-  APP_TOKEN=$(echo "$APP_TOKEN_JSON" | sudo sed -n "s/.*\"appToken\":\"\([^\"]*\)\".*/\1/p")
-
-  if [ -z "$APP_TOKEN" ]; then
-    echo "Không có appToken được trả về, chờ 2 phút..."
-    sleep 120
-    continue
-  fi
-
-  echo "Nhận được appToken: $APP_TOKEN"
 
   # Đảm bảo thư mục tồn tại trước khi làm việc
   if [ ! -d "InternetIncome-main" ]; then
@@ -46,22 +33,9 @@ while true; do
     continue
   fi
 
-  # So sánh token cũ và mới
-  CURRENT_TOKEN=$(sed -n 's/^TRAFFMONETIZER_TOKEN=//p' properties.conf | tr -d '\r')
-  echo "CURRENT_TOKEN: [$CURRENT_TOKEN]"
-  echo "NEW_APP_TOKEN: [$APP_TOKEN]"
-
-  TOKEN_CHANGED=false
-  if [ "$CURRENT_TOKEN" != "$APP_TOKEN" ]; then
-    TOKEN_CHANGED=true
-    echo "Token thay đổi → cập nhật"
-    sudo sed -i "s|^TRAFFMONETIZER_TOKEN=.*|TRAFFMONETIZER_TOKEN=$APP_TOKEN|" properties.conf
-  else
-    echo "Token giống nhau"
-  fi
-
   sudo sed -i "s|^USE_PROXIES=.*|USE_PROXIES=true|" properties.conf
   sudo sed -i "s|^CASTAR_SDK_KEY=.*|CASTAR_SDK_KEY=cskLEggSnhicxN|" properties.conf
+  sudo sed -i 's|^TRAFFMONETIZER_TOKEN=.*|TRAFFMONETIZER_TOKEN=OeWo3xtxe5TusWwkTdsjfpoulyY3l9CqOMez01eZK/s=|' properties.conf
 
   # Kiểm tra container
   # Lấy danh sách container đang chạy
@@ -71,7 +45,7 @@ while true; do
   if [ "$CONTAINER_COUNT" -ge 4 ]; then
     echo "Có $CONTAINER_COUNT container đang chạy"
 
-    if [ "$TOKEN_CHANGED" = true ] || [ "$HAS_PROXY_UPDATE" = true ]; then
+    if [ "$HAS_PROXY_UPDATE" = true ]; then
       echo "Có thay đổi token/proxy → restart"
       sudo docker ps -q | sudo xargs -n1 docker update --restart=no
       sudo bash internetIncome.sh --delete
